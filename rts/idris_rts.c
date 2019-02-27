25d24dabadc67a34e8a66296765c9b1dc0855b64
Jonas Claeson <jonas.cl@protonmail.com>
2019-02-27 18:36:08 +0100

    Create idris_pthread.*

--- rts/idris_rts.c
+++ rts/idris_rts.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <errno.h>
+#include <string.h>
 
 #include "idris_rts.h"
 #include "idris_gc.h"
@@ -25,37 +26,6 @@ static VM* global_vm;
 void free_key(void *vvm) {
     // nothing to free, we just used the VM pointer which is freed elsewhere
 }
-
-#ifdef HAS_PTHREAD
-void init_vm_pthread(
-    VMPthread *pt,
-    int max_threads // not implemented yet
-    ) {
-    pt->inbox = malloc(1024*sizeof(pt->inbox[0]));
-    assert(pt->inbox);
-    memset(pt->inbox, 0, 1024*sizeof(pt->inbox[0]));
-    pt->inbox_end = pt->inbox + 1024;
-    pt->inbox_write = pt->inbox;
-    pt->inbox_nextid = 1;
-
-    // The allocation lock must be reentrant. The lock exists to ensure that
-    // no memory is allocated during the message sending process, but we also
-    // check the lock in calls to allocate.
-    // The problem comes when we use requireAlloc to guarantee a chunk of memory
-    // first: this sets the lock, and since it is not reentrant, we get a deadlock.
-    pthread_mutexattr_t rec_attr;
-    pthread_mutexattr_init(&rec_attr);
-    pthread_mutexattr_settype(&rec_attr, PTHREAD_MUTEX_RECURSIVE);
-
-    pthread_mutex_init(&(pt->inbox_lock), NULL);
-    pthread_mutex_init(&(pt->inbox_block), NULL);
-    pthread_mutex_init(&(pt->alloc_lock), &rec_attr);
-    pthread_cond_init(&(pt->inbox_waiting), NULL);
-
-    pt->max_threads = max_threads;
-    pt->processes = 0;
-}
-#endif
 
 VM* init_vm(int stack_size, size_t heap_size,
             int max_threads // not implemented yet
