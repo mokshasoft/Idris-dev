7678f65c7b3ea93523da98ef64e3a559a66a94cb
Jonas Claeson <jonas.cl@protonmail.com>
2020-02-14 08:00:01 +0100

    freertos: Add idris_main

---
+++ rts/FreeRTOS/idris_FreeRTOS.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020, Mokshasoft AB (mokshasoft.com)
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ */
+
+#include "idris_FreeRTOS.h"
+#include "idris_rts.h"
+
+void wrapper_vTaskDelay(int delay_ms)
+{
+    vTaskDelay(delay_ms/portTICK_RATE_MS);
+}
+
+QueueHandle_t wrapper_xQueueCreate(UBaseType_t uxQueueLength)
+{
+    return xQueueCreate(uxQueueLength, sizeof(void*));
+}
+
+void wrapper_vQueueDelete(QueueHandle_t xQueue)
+{
+    vQueueDelete(xQueue);
+}
---
+++ rts/FreeRTOS/idris_FreeRTOS.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2020, Mokshasoft AB (mokshasoft.com)
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ */
+
+#include <FreeRTOS.h>
+#include <queue.h>
+
+void wrapper_vTaskDelay(int delay_ms);
+
+QueueHandle_t wrapper_xQueueCreate(UBaseType_t uxQueueLength);
+void wrapper_vQueueDelete(QueueHandle_t xQueue);
